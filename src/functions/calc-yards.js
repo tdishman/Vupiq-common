f6aa5d60e90a51a940b3bee3728e7725d8384b8b
Vasily Kiniv <yuberion@gmail.com>
2017-07-22 22:50:36 +0300

    Fix calcYards

--- src/functions/calc-yards.js
+++ src/functions/calc-yards.js
@@ -1,14 +1,6 @@
-import { playTypes } from '../constants';
-
-const {
-  TYPE_FIELDGOAL,
-  TYPE_EXTRA_POINT
-} = playTypes;
-
 function calcYards(play) {
-  const { start, end, home, away, base } = play;
+  const { start, end, away } = play;
 
-  const possTeam = end.possession === 'home' ? home.alias : away.alias;
   const away2Absolute = location => (50 - location) + 50;
   const startLoc = start.locationAlias === away.alias ? away2Absolute(start.location) : start.location;
   const endLoc = end.locationAlias === away.alias ? away2Absolute(end.location) : end.location;
@@ -16,26 +8,6 @@ function calcYards(play) {
   let gained = endLoc - startLoc;
   if (start.possession === away.alias) {
     gained *= -1;
-  }
-
-  if (start.locationAlias === end.locationAlias) {
-    switch (base.type) {
-      case TYPE_EXTRA_POINT:
-        gained = 32;
-        break;
-
-      case TYPE_FIELDGOAL:
-        if (start.locationAlias === possTeam) {
-          gained = (50 - start.location) + 67;
-        }
-        else {
-          gained = start.location + 17;
-        }
-        break;
-
-      default:
-        break;
-    }
   }
 
   return gained;
