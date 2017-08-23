fab7d09b92d9ca35cc7b81a6c1f7172bdfc622e8
alex <barbarosh@ukr.net>
2017-08-23 17:25:01 +0300

    Add additional play types.

--- src/constants/play-types.js
+++ src/constants/play-types.js
@@ -1,13 +1,39 @@
-export const TYPE_NONE = 'NONE';
+/**
+ * Sportradar play types
+ * @type {string}
+ */
 export const TYPE_PASS = 'PASS';
 export const TYPE_RUSH = 'RUSH';
-export const TYPE_PENALTY = 'PENALTY';
-export const TYPE_PUNT = 'PUNT';
-export const TYPE_FIELDGOAL = 'FIELDGOAL';
-export const TYPE_KICKOFF = 'KICKOFF';
+export const TYPE_FAIRCATCH_KICK = 'FAIRCATCH_KICK';
 export const TYPE_EXTRA_POINT = 'EXTRA_POINT';
+export const TYPE_CONVERSION = 'CONVERSION';
+export const TYPE_FREE_KICK = 'FREE_KICK';
+export const TYPE_KICKOFF = 'KICKOFF';
+export const TYPE_PUNT = 'PUNT';
+export const TYPE_FIELD_GOAL = 'FIELD_GOAL';
+export const TYPE_PENALTY = 'PENALTY';
+
+/**
+ * Sportradar event types
+ * @type {string}
+ */
+export const TYPE_SETUP = 'SETUP';
+export const TYPE_TIMEOUT = 'TIMEOUT';
+export const TYPE_COMMENT = 'COMMENT';
+export const TYPE_PERIOD_END = 'PERIOD_END';
+export const TYPE_GAME_OVER = 'GAME_OVER';
+
+/**
+ * Custom play types
+ * @type {string}
+ */
+export const TYPE_NONE = 'NONE';
 export const TYPE_SACK = 'SACK';
 
+/**
+ * Custom score types
+ * @type {string}
+ */
 export const SCORE_NONE = 'NONE';
 export const SCORE_OFF_TOUCHDOWN = 'OFF_TOUCHDOWN';
 export const SCORE_DEF_TOUCHDOWN = 'DEF_TOUCHDOWN';
@@ -17,6 +43,10 @@ export const SCORE_EXTRA_POINT = 'EXTRA_POINT';
 export const SCORE_TWOPT_CONVERSION = 'TWOPT_CONVERSION';
 export const SCORE_SAFETY = 'SAFETY';
 
+/**
+ * Custom turnover types
+ * @type {string}
+ */
 export const TURNOVER_NONE = 'NONE';
 export const TURNOVER_UNDEFINED = 'UNDEFINED';
 export const TURNOVER_FUMBLE = 'FUMBLE';
