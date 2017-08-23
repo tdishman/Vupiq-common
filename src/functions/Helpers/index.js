f24b92a91f5a241d316b848b8bb1a4a7812f1cfd
alex <barbarosh@ukr.net>
2017-08-23 23:27:56 +0300

    Add getInitialPlay helper

--- src/functions/Helpers/index.js
+++ src/functions/Helpers/index.js
@@ -5,6 +5,36 @@ export const playIsScoring = play => [playTypes.TYPE_RUSH, playTypes.TYPE_PASS, 
 
 export const gameIsActive = game => [gameConstants.INPROGRESS, gameConstants.HALFTIME].indexOf(game.info.status) > -1;
 
+export const getInitialPlay = () => {
+  return {
+    processed: false,
+    rejected: false,
+    fulfilled: false,
+    merged: false,
+    issued: false,
+    away: {
+      alias: '',
+      score: 0
+    },
+    home: {
+      alias: '',
+      score: 0
+    },
+    base: {
+      type: playTypes.TYPE_NONE,
+      scoring: playTypes.SCORE_NONE,
+      turnover: playTypes.TURNOVER_NONE,
+      yardsGained: 0,
+      description: ''
+    },
+    points: {
+      rush: 0,
+      def: 0,
+      pass: 0
+    }
+  };
+};
+
 export const toOrdinal = value => {
   if ([11, 12, 13].indexOf(value % 100) > -1) {
     return value + 'th';
